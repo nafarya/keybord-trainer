c61bf4e690a928283c4f1b407e344a7564aeb103
Danil Yaschenko <danyaschenko@gmail.com>
2016-03-30 01:27:31 +0300

    added translations

---
+++ arrowpad_en.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>KeyBoard Trainer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>Points</source>
+        <translation>Points</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="100"/>
+        <source>Time Remaing</source>
+        <translation>Time remaining</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>15 SEC TOP </source>
+        <translation>15 SEC TOP</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="175"/>
+        <source>30 SEC TOP</source>
+        <translation>30 SEC TOP</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="188"/>
+        <source>45 SEC TOP</source>
+        <translation>45 SEC TOP</translation>
+    </message>
+</context>
+</TS>
---
+++ arrowpad_ru.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Клавиатурный тренажер</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Start</source>
+        <translation>Старт</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>Points</source>
+        <translation>Очки</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="100"/>
+        <source>Time Remaing</source>
+        <translation>Осталось времени</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>15 SEC TOP </source>
+        <translation>Лидер 15 с</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="175"/>
+        <source>30 SEC TOP</source>
+        <translation>Лидер 30 с</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="188"/>
+        <source>45 SEC TOP</source>
+        <translation>Лидер 45 с</translation>
+    </message>
+</context>
+</TS>
